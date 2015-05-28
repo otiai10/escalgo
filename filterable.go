744d717fc7db02a5fd36f2006e7cacdeb0563ecd
otiai10 <otiai10@gmail.com>
2015-05-28 19:10:00 +0900

    wip filterable

--- filterable.go
+++ filterable.go
@@ -1,4 +1,16 @@
 package escalgo
 
-// Filterable can be a filter.
-type Filterable interface{}
+// Filterable can be a child node under "filter".
+// See https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-filters.html
+//   - [ ] Compoundable: is possible to include UNSPECIFIED and PLURAL OF filterables.
+//     - [ ] and
+//     - [ ] bool
+//   - [ ] Filterable Element: is SPECIFIED filter element.
+//   - [ ] Others: TODO :(
+//     - [ ] constant_score
+//     - [ ] nested
+//     - [ ] minimum_should_match
+//     - [ ] rewirte
+type Filterable interface {
+	MarshalFilter() ([]byte, error)
+}
